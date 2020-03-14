45d61e82a4124022dea321ff0bb06a271fb42b1b
kayla9394 <noronha.kayla@gmail.com>
2020-03-14 15:24:28 -0400

    added objects to objects in data file and links

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,26 @@
+var context = {
+  header: {
+    name: "Kayla",
+    title: "Full Stack Developer",
+    email: "knoronha@",
+    mobile: "226",
+    location: "Toronto,ON",
+    git: {
+      link: "https://github.com/K-Noronha",
+      title: "Github.com/K-Noronha"
+    }
+  },
+  summary:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+};
 
-var context = { "name": "Kayla", "title": "Full Stack Developer", "email": "knoronha@", "mobile": "226"}
 
+//Compliles the template
 var source = document.getElementById("entry-template").innerHTML;
 var template = Handlebars.compile(source);
 
-
+//Gets the resulting HTML
 var updatedhtml = template(context);
 
-
+//Updates the DOM
 document.getElementById("content-placeholder").innerHTML = updatedhtml;
